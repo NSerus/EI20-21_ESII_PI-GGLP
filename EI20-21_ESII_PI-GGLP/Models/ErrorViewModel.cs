7ac0fe2f07f22f8ebb6132e490f68174b62a6525
José Costa <j.ricardo.n.costa@gmail.com>
2020-11-02 10:59:01 +0000

    Add project files.

---
+++ EI20-21_ESII_PI-GGLP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EI20_21_ESII_PI_GGLP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
