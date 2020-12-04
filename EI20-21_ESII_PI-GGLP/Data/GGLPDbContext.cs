c9aede3914c545759272ea54130764823f993aa2
José Costa <j.ricardo.n.costa@gmail.com>
2020-12-04 09:22:37 +0000

    -Create DbContext

---
+++ EI20-21_ESII_PI-GGLP/Data/GGLPDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EI20_21_ESII_PI_GGLP.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EI20_21_ESII_PI_GGLP.Data
+{
+    public class GGLPDbContext : DbContext
+    {
+        public GGLPDbContext(DbContextOptions<GGLPDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<Pontos> Pontos { get; set; }
+
+        //public DbSet<Contacts> Contactos { get; set; }
+    }
+}
