b0331a022d8be911f0d78cb049e9c754cbcfb686
José Costa <j.ricardo.n.costa@gmail.com>
2020-12-18 09:18:25 +0000

    -actualização das classes: 'PontoDeInteresse'; -alterações no layout do scafollding; -etc...

--- EI20-21_ESII_PI-GGLP/Models/Pontos.cs
+++ EI20-21_ESII_PI-GGLP/Models/Pontos.cs
@@ -92,7 +92,7 @@ namespace EI20_21_ESII_PI_GGLP.Models
         // COVID
         [Required(ErrorMessage = "Introduza as normas COVID-19 aplicadas no novo Ponto de Interesse.")]
         [Display(Name = "COVID-19")]
-        [StringLength(400, MinimumLength = 10, ErrorMessage = "Mínimo 10 e máximo de 400 caracters.")]
+        [StringLength(400, ErrorMessage = "Mínimo 10 e máximo de 400 caracters.")]
         public string PCovid { get; set; }
     }
 }
