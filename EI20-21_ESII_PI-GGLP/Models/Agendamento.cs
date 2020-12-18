2a659c9ab6d8ef7dba1e712852f60c8a97880095
NSerus <nunoworkingserus@gmail.com>
2020-12-18 09:17:13 +0000

    Changed Agendamento to be like ER model

---
+++ EI20-21_ESII_PI-GGLP/Models/Agendamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EI20_21_ESII_PI_GGLP.Models
+{
+    public class Agendamento
+    {
+        [Key]
+        public int AgendamentoId { get; set; }
+
+        public string AData { get; set; }
+
+        [Required(ErrorMessage = "Selecione a Hora")]
+        public string AHora { get; set; }
+        [Required(ErrorMessage = "Escreva o número de pessoas que o vai acompanhar")]
+        [Range(1, 4, ErrorMessage = "O número do grupo tem que ser de 1 a 4 pessoas")]
+        public int ANumPessoas { get; set; }
+    }
+}
