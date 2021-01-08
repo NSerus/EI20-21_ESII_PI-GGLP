8b018b9b7fdd9b1125db3707c8bc806dccb7424d
NSerus <nunoworkingserus@gmail.com>
2021-01-08 09:39:37 +0000

    Added new tables

---
+++ EI20-21_ESII_PI-GGLP/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EI20_21_ESII_PI_GGLP.Models
+{
+    public class Categoria
+    {
+    }
+}
