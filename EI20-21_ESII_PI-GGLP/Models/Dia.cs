719eef707c8446c6f4bfb6dbac787763552ce4a5
José Costa <j.ricardo.n.costa@gmail.com>
2021-01-08 10:02:29 +0000

    R1CH4RD5-Refresh

---
+++ EI20-21_ESII_PI-GGLP/Models/Dia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EI20_21_ESII_PI_GGLP.Models
+{
+    public class Dia
+    {
+        // Dia_ID (PK)
+        [Key]
+        public int Dia_ID { get; set; }
+
+        // DNome
+        [Required(ErrorMessage = "Nome do Dia.")]
+        [Display(Name = "DNome")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "O nome deverá ter no mínimo 5 caracters.")]
+        public string DNome { get; set; }
+
+        // DComments
+
+    }
+}
