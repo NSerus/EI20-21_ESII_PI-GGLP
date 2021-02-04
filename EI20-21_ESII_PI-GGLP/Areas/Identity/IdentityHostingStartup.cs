6c95406f4b5f4a811dfb75a8e11d31be3ec6dec4
José Costa <j.ricardo.n.costa@gmail.com>
2021-02-04 21:25:05 +0000

    R1CH4RD5-ChangesAndCorrections

---
+++ EI20-21_ESII_PI-GGLP/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EI20_21_ESII_PI_GGLP.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EI20_21_ESII_PI_GGLP.Areas.Identity.IdentityHostingStartup))]
+namespace EI20_21_ESII_PI_GGLP.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
